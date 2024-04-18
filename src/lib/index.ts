ede916e9c9e8a44af8703a97197ed656bcf1d0ee
Maxim <maximgratii@gmail.com>
2024-04-18 15:29:45 +0300

    fix

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,18 +1,22 @@
+import { onMount } from "svelte";
+
 export function initFeedefyWidget(projectId: string, lang = '') {
-  if (!projectId) {
-    console.error('No projectId was passed to Feedefy Svelte, widget will not be initialized');
-    return;
-  }
-
-  const script = document.createElement('script');
-
-  if (lang) {
-    script.lang = lang;
-  }
-
-  script.src = `https://app.feedefy.com/embed.js?id=${projectId}`;
-  script.defer = true;
-  script.addEventListener('error', () => script.remove());
-
-  document.body.appendChild(script);
+  onMount(() => {
+    if (!projectId) {
+      console.error('No projectId was passed to Feedefy Svelte, widget will not be initialized');
+      return;
+    }
+  
+    const script = document.createElement('script');
+  
+    if (lang) {
+      script.lang = lang;
+    }
+  
+    script.src = `https://app.feedefy.com/embed.js?id=${projectId}`;
+    script.defer = true;
+    script.addEventListener('error', () => script.remove());
+  
+    document.body.appendChild(script);
+  });
 }
